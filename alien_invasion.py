28f3f8da8b9b5ed86262896978103cd7bcb3e075
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-16 04:55:04 +0300

    call settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,16 @@
+
 import pygame
 import sys
+from settings import Settings
+
 
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
-
-        # set background color.
-        self.bg_color = (0,0,255)
 
     def run_game(self):
         while True:
@@ -18,7 +19,7 @@ class AlienInvasion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop.      
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
     
