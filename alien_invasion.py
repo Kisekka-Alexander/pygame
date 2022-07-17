5ca7e30ea210d92f972000c2bbb7a59ca2b99134
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-17 22:28:11 +0300

    modify screen size

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,7 +22,9 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.      
             self.screen.fill(self.settings.bg_color)
+            
             self.ship.blitme()
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
     
