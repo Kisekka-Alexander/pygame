02b76973a921c93ab7f2a82ee6d916323b62b0aa
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-18 01:52:13 +0300

    adjust ship position

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,6 +17,7 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_events()
             
     def _check_events(self):
@@ -26,7 +27,15 @@ class AlienInvasion:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
                         # Move the ship to the right
-                        self.ship.rect.x += 1
+                        self.ship.moving_right = True
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 
     def _update_events(self):
         """Update images on the screen, and flip to the new screen."""
